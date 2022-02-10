d87e2e5d5b3fd6761cdb079e1ba12805a6218a26
ginayana <97510602+ginayana@users.noreply.github.com>
2022-02-09 16:16:37 -1000

    Completed Part 5 Problem Sets

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Parts 1-3
 var goodGrade = "I've been doing all my work"
 if goodGrade == "I've been doing all my work"
 {
@@ -11,6 +13,7 @@ else
     print("I need to work harder in this class")
 }
 
+//Part 4
 var gradeA = true
 if gradeA == true
 {
@@ -19,4 +22,42 @@ if gradeA == true
 else
 {
     print("I'm not so sure about coding in Swift")
+}
+
+//Problem Set 1:
+var A = 90
+var B = 80
+if B > A
+{
+    print("Excellent work!")
+}
+else
+{
+    print("Need to work harder")
+}
+
+//Problem Set 2:
+var happy = "Awesome!"
+var sad = "Junk!"
+if happy == "Awesome!"
+{
+    print("Optimistic")
+}
+else
+{
+    print("Pessimistic")
+}
+
+
+//Problem Set 3:
+var dogCat = true
+var catDog = false
+
+if dogCat == false
+{
+    print("Dogs rule!")
+}
+else
+{
+    print("Cats rule!")
 }
